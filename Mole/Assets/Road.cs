4dc2a1711c6b615ac38b8c48e306580116467a77
cool1259 <cool1259@nate.com>
2025-03-09 21:24:11 +0900

    정리 (bonroad필요x)

--- Mole/Assets/Road.cs
+++ Mole/Assets/Road.cs
@@ -6,25 +6,12 @@ public class Road : MonoBehaviour
 {
 
     [SerializeField] Collider2D collider_;
-    public bool isNoUseRoad = true; 
+    
+
     public void DisableCollider()
     {
-        isNoUseRoad = false;
         gameObject.layer = LayerMask.NameToLayer("FinishRoad");
 
         // collider_.enabled = false;
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (gameObject.layer == LayerMask.NameToLayer("FinishRoad"))
-            return;
-
-        if (collision.gameObject.layer != LayerMask.NameToLayer("Player"))
-            return;
-
-        
-        gameObject.layer = LayerMask.NameToLayer("Road");
-
     }
 }
