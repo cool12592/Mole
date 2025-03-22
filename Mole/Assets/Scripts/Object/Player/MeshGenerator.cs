87c7c504edea54842bd8008b2aac883949cae59b
cool1259 <cool1259@nate.com>
2025-03-23 00:16:02 +0900

    0킬

--- Mole/Assets/Scripts/Object/Player/MeshGenerator.cs
+++ Mole/Assets/Scripts/Object/Player/MeshGenerator.cs
@@ -101,7 +101,9 @@ public class MeshGenerator : MonoBehaviourPunCallbacks
         PV = GetComponent<PhotonView>();
         AssignColor();
 
-        myKillText = GameObject.Find("Canvas").transform.Find("Gaming").transform.Find("Kill").transform.Find("MyKillCount").GetComponent<Text>();  
+        myKillText = GameObject.Find("Canvas").transform.Find("Gaming").transform.Find("Kill").transform.Find("MyKillCount").GetComponent<Text>();
+
+        myKillText.text = "0 Kill";
     }
 
     private IEnumerator Start()
