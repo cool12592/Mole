63d3a4efed79d0e01e4a1a41ec8a9f22ba7673da
alfie <alfie@supercent.io>
2025-03-28 02:52:42 +0900

    myga수정

--- Mole/Assets/MyGa.cs
+++ Mole/Assets/MyGa.cs
@@ -5,7 +5,21 @@ using UnityEngine;
 
 public class MyGa : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private static MyGa instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // 씬 전환 시 파괴되지 않음
+        }
+        else
+        {
+            Destroy(gameObject); // 중복된 경우 삭제 (씬 재로드로 인해 새로 생성된 경우)
+        }
+    }
+
     void Start()
     {
         GameAnalytics.Initialize();
